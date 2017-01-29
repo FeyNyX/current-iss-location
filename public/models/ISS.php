48ce33880f5ee42682068b4418d922c29e34e76e
VoyteckLenartovich <git@github.com>
2017-01-29 23:18:56 +0100

    additional comment

--- public/models/ISS.php
+++ public/models/ISS.php
@@ -22,6 +22,17 @@ class ISS
         * The access to Google Maps API is not completely free,
         * so we don't want our API key to be publicly displayed to everbody on the client side.
         * That's why we are querying the API on our server.
+        *
+        * Additional thing that should be implemented here would be querying Google Maps API
+        * only once per every 10 seconds and saving the results to our own database.
+        * This would save us making independent (costly) queries for every user that visits our site.
+        *
+        * Step by step:
+        * - user enters the site and we check if we have actual position in our database
+        * - if not, we query the API and save fresh data to our database
+        *
+        * We could set cron instead, but that way we could make unneeded queries to the API,
+        * even when there are no visitors on our site at all at given moment.
         */
         $json = json_decode(
             file_get_contents(
